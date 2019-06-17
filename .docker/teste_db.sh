4d0a5e2e9412e95c7b7b06af65b6e66781b15c50
Lucas Otávio Teodoro Silva <lucas.teodoro@lotsystem.com.br>
2019-06-17 10:28:31 -0300

    .gitignore docker-compose.yaml teste_db.sh

--- .docker/teste_db.sh
+++ .docker/teste_db.sh
@@ -5,12 +5,7 @@ set -e
 
 host="$1"
 shift
-cmd="$@"
 
 until MYSQL_PASSWORD=$MYSQL_PASSWORD mysql -h "$host" -u "root" -c '\q'; do
-  >&2 echo "Mysql is unavailable - sleeping"
   sleep 1
 done
-
->&2 echo "Mysql is up - executing command"
-exec $cmd
