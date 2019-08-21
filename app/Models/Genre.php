1e578eef5abcd67179b51c2c648339d7b58daeeb
Lucas Otávio Teodoro Silva <lucas.teodoro@lotsystem.com.br>
2019-08-21 16:49:17 -0300

    Category Genre

---
+++ app/Models/Genre.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Genre extends Model
+{
+    use SoftDeletes, Uuid;
+    protected $fillable = ["name"];
+    protected $dates = ["deleted_at"];
+    protected $casts = ["id" => "string"];
+}
