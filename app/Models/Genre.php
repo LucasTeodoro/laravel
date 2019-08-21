e67c65a1a3ffe913132239a247f15a8f4b5a3ee4
Lucas Otávio Teodoro Silva <lucas.teodoro@lotsystem.com.br>
2019-08-21 17:22:30 -0300

    incrementing false

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -12,4 +12,5 @@ class Genre extends Model
     protected $fillable = ["name"];
     protected $dates = ["deleted_at"];
     protected $casts = ["id" => "string"];
+    public $incrementing = false;
 }
