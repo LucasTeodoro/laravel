8c456f332cfefe6d85456c5af0943973f07a7c61
Lucas Otávio Teodoro Silva <lucasotavio53@hotmail.com>
2021-06-07 00:27:40 -0300

    frontend

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+npm config set cache /var/www/.npm-cache --global
+
+cd /var/www/frontend && npm install && cd ..
+
+cd backend
+
 cp .env.example .env
 cp .env.testing.example .env.testing
 composer install --ignore-platform-reqs --no-interaction --no-plugins --no-scripts --prefer-dist
